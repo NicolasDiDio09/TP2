5df19a626535c73766535f90e4e9fbb5cabceaf6
Matias <santolarimatias@gmail.com>
2021-10-09 00:54:39 -0300

    capa de entidad, de datos y la logica agregadas al abm especialidad

---
+++ Data.Database/EspecialidadAdapter.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Business.Entities;
+
+namespace Data.Database
+{
+    public class EspecialidadAdapter : Adapter
+    {
+
+        public List<Especialidad> GetAll()
+        {
+            List<Especialidad> especialidades = new List<Especialidad>();
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdEspecialidades = new SqlCommand("select * from especialidades", sqlConn);
+                SqlDataReader drEspecialidades = cmdEspecialidades.ExecuteReader();
+                while (drEspecialidades.Read())
+                {
+                    Especialidad esp = new Especialidad();
+                    esp.ID = (int)drEspecialidades["id_especialidad"];
+                    esp.DescEspecialidad = (string)drEspecialidades["desc_especialidad"];
+                    especialidades.Add(esp);
+                }
+                drEspecialidades.Close();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al recuperar lista de especialidades", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+            return especialidades;
+        }
+
+        public Especialidad GetOne(int ID)
+        {
+            Especialidad esp = new Especialidad();
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdEspecialidad = new SqlCommand("Select * from especialidades where id_especialidad = @id", sqlConn);
+                cmdEspecialidad.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                SqlDataReader drEspecialidades = cmdEspecialidad.ExecuteReader();
+                if (drEspecialidades.Read())
+                {
+                    esp.ID = (int)drEspecialidades["id_especialidad"];
+                    esp.DescEspecialidad = (string)drEspecialidades["desc_especialidad"];
+                }
+                drEspecialidades.Close();
+            }
+            catch (Exception e)
+            {
+                Exception ExcepcionManejada = new Exception("Error al recuperar datos de la especialidad", e);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+            return esp;
+        }
+        protected void Update(Especialidad especialidad)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdSave = new SqlCommand("UPDATE especialidades SET desc_especialidad=@desc_especialidad where id_especialidad=@id", sqlConn);
+                cmdSave.Parameters.Add("@id", SqlDbType.Int).Value = especialidad.ID;
+                cmdSave.Parameters.Add("@desc_especialidad", SqlDbType.VarChar, 50).Value = especialidad.DescEspecialidad;
+                cmdSave.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Exception er = new Exception("Error al actualizar los datos de la especialidad", e);
+                throw er;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        public void Delete(int ID)
+        {
+            try
+            {
+                //abrimos la conexión
+                this.OpenConnection();
+
+                //creamos la sentencia sql y asignamos un valor al parámetro
+                SqlCommand cmdDelete = new SqlCommand("delete especialidades where id_especialidad=@id", sqlConn);
+                cmdDelete.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                cmdDelete.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al eliminar la especialidad", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+        protected void Insert(Especialidad especialidad)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdSave = new SqlCommand(
+                "insert into especialidades(desc_especialidad) " +
+                "Values(@desc_especialidad)", sqlConn);
+                cmdSave.Parameters.Add("@desc_especialidad", SqlDbType.VarChar, 50).Value = especialidad.DescEspecialidad;
+                cmdSave.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al crear especialidad", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        public void Save(Especialidad especialidad)
+        {
+            if (especialidad.State == BusinessEntity.States.Deleted)
+            {
+                this.Delete(especialidad.ID);
+            }
+            else if (especialidad.State == BusinessEntity.States.New)
+            {
+                this.Insert(especialidad);
+            }
+            else if (especialidad.State == BusinessEntity.States.Modified)
+            {
+                this.Update(especialidad);
+            }
+            especialidad.State = BusinessEntity.States.Unmodified;
+        }
+    }
+}
